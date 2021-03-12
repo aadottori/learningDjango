b318b1518324a16db4a883e215b6b5661d739314
aadottori <37637976+aadottori@users.noreply.github.com>
2021-03-12 00:43:49 -0300

    Rendering Templates; Base html

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#add new applications here everytime we create something
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': "Antonio Dottori",
+        'title': "How to create a blog post 101",
+        'content': "First post here.",
+        'date_posted': "February 04, 2021"
+    },
+    {
+        'author': "João da Silva",
+        'title': "Posting on blogs",
+        'content': "Second post here.",
+        'date_posted': "March 05, 2021"
+    }
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return HttpResponse("<p>About</p>")
+    return render(request, "blog/about.html", {"title": "about"})
+
+
+# outra forma de criar a rota
+# def home(request):
+#    return HttpResponse("<h1>Blog Home</h1>")
